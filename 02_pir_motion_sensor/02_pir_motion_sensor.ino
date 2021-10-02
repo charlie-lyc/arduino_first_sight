0aa9a9a538f42d4afbfb9b36416ada1de7cedbf1
charlie_lyc <ofetwo@gmail.com>
2021-10-03 03:34:22 +0900

    led and motion sensor control

---
+++ 02_pir_motion_sensor/02_pir_motion_sensor.ino
@@ -0,0 +1,19 @@
+int val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(2);
+  if (val == HIGH) {
+    digitalWrite(3, HIGH);
+  } else {
+    digitalWrite(3, LOW);
+  }
+  Serial.println(val);
+}
